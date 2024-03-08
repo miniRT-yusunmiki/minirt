e463e3633283378a9e8d15af33a801b0a24d08fe
jin20fd <yusung@c5r8s5.42seoul.kr>
2024-03-08 22:08:07 +0900

    Fix: sphere 내부에 카메라 있는 경우를 위해 t값 2개 모두 구하기

--- src/hit/hit_sphere.c
+++ src/hit/hit_sphere.c
@@ -18,7 +18,11 @@ t_bool	hit_sphere(t_sphere *sp, t_ray *ray, t_hit_record *rec)
 	thc = sqrt(sp->radius2 - pow(d, 2));
 	t = tca - thc;
 	if (t < rec->tmin || t > rec->tmax)
-		return (FALSE);
+	{
+		t = tca + thc;
+		if (t < rec->tmin || t > rec->tmax)
+			return (FALSE);
+	}
 	rec->t = tca - thc;
 	rec->p = ray_at(ray, t);
 	rec->normal = vdivide(vminus(rec->p, sp->center), sp->radius);
