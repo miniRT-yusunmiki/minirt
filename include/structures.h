ef9e88bf3201a9c0d9b607b49fefbde608009c94
seonmiki <seonmiki@student.42seoul.kr>
2024-03-11 13:53:49 +0900

    Fix: 카메라가 구 안에 들어갔을 때, hit_sphere 예외처리

--- include/structures.h
+++ include/structures.h
@@ -24,6 +24,7 @@ typedef struct s_light t_light;
 typedef struct s_sphere t_sphere;
 typedef struct s_plane t_plane;
 typedef struct s_cylinder t_cylinder;
+typedef struct s_sp_cal t_sp_cal;
 typedef struct s_formula t_formula;
 
 typedef struct s_cnt	t_cnt;
@@ -162,6 +163,14 @@ struct s_cylinder
 	double		radius;
 	double		height;
 	t_color3	color;
+};
+
+struct s_sp_cal
+{
+	t_vec3 	L;
+	double	tca;
+	double	thc;
+	double	d;
 };
 
 struct s_formula
--- src/hit/hit_sphere.c
+++ src/hit/hit_sphere.c
@@ -1,30 +1,46 @@
 #include "../../include/minirt.h"
 
+static void	ft_swap(double *a, double *b)
+{
+	double	tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+static t_bool	sp_calculate(t_sphere *sp, t_ray *ray, t_sp_cal *sp_cal)
+{
+	sp_cal->L = vminus(sp->center, ray->orig);
+	sp_cal->tca = vdot(sp_cal->L, ray->dir);
+	sp_cal->d = sqrt(vlength2(sp_cal->L) - pow(sp_cal->tca, 2));
+	if (sp_cal->d > sp->radius)
+		return (FALSE);
+	sp_cal->thc = sqrt(sp->radius2 - pow(sp_cal->d, 2));
+	return (TRUE);
+}
+
 t_bool	hit_sphere(t_sphere *sp, t_ray *ray, t_hit_record *rec)
 {
-	double	tca;
-	double	thc;
-	double	d;
-	double	t;
-	t_vec3	L;
+	t_sp_cal	sp_cal;
+	double		t[2];
+	double		root;
  
-	L = vminus(sp->center, ray->orig);
-	tca = vdot(ray->dir, L);
-	if (tca < 0)
+	if (sp_calculate(sp, ray, &sp_cal) == FALSE)
 		return (FALSE);
-	d = sqrt(vlength2(L) - pow(tca, 2));
-	if (d > sp->radius)
-		return (FALSE);
-	thc = sqrt(sp->radius2 - pow(d, 2));
-	t = tca - thc;
-	if (t < rec->tmin || t >= rec->tmax)
+	t[0] = sp_cal.tca - sp_cal.thc;
+	t[1] = sp_cal.tca + sp_cal.thc;
+	if (t[0] > t[1])
+		ft_swap(&t[0], &t[1]);
+	root = t[0];
+	if (root < rec->tmin || root >= rec->tmax)
 	{
-		t = tca + thc;
-		if (t < rec->tmin || t >= rec->tmax)
+		root = t[1];
+		if (root < rec->tmin || root >= rec->tmax)
 			return (FALSE);
 	}
-	rec->t = t;
-	rec->p = ray_at(ray, t);
+	rec->t = root;
+	rec->p = ray_at(ray, root);
 	rec->normal = vunit(vminus(rec->p, sp->center));
 	rec->color = sp->color;
 	set_face_normal(ray, rec);
